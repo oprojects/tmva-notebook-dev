3f181631df91e60f754d4dba8ecadc946fdc63b0
Omar Zapata <Omar.Zapata@cern.ch>
2016-08-12 17:55:40 +0200

    initial vertions of notebooks

---
+++ cvmpi.C
@@ -0,0 +1,37 @@
+#include <TMVA/ParallelExecutorMpi.h>
+#include <TMVA/DataLoader.h>
+#include <TMVA/Tools.h>
+#include <TMVA/CrossValidation.h>
+
+#include <TMessage.h>
+
+void cvmpi()
+{
+    TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
+    TMVA::ParallelExecutorMpi executor;
+    
+    //setting up the dataloader in the main process
+    if(executor.IsMainProcess()){
+        TFile file("dataset.root");
+        dataloader=(TMVA::DataLoader *)file.Get("dataset");
+        file.Close();
+    }
+    
+    //if it is the main process it will be send else it will be reciev
+    executor.SharedDataLoader(dataloader);
+    
+    TMVA::CrossValidation *cv=new TMVA::CrossValidation(dataloader);
+
+    cv->BookMethod(TMVA::Types::kBDT, "BDT",
+    "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:nCuts=20");
+   
+    cv->SetNumFolds(8);
+  
+    auto results=executor.Execute(cv);
+
+    if(executor.IsMainProcess()){
+        TMVA::MsgLogger::EnableOutput();
+        TMVA::gConfig().SetSilent(kFALSE);   
+        results.Print();
+    }
+}
